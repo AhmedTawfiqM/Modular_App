23b0d1f70c5ff3336e9263bd1a9984c4ce9bed16
Tawfiq <=>
2021-09-25 01:32:04 +0200

    kodein injector integration

--- app/src/main/java/com/app/modularApp/shared/App.kt
+++ app/src/main/java/com/app/modularApp/shared/App.kt
@@ -4,12 +4,18 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import androidx.annotation.StringRes
+import com.app.modularApp.shared.di.KodeinInjector
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         context = this
+        setup()
+    }
+
+    private fun setup() {
+        KodeinInjector.inject(this)
     }
 
     companion object {
