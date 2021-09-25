1ee2d291bcca3850d2ae3c94cdbaadf9dce53778
Tawfiq <=>
2021-09-25 02:33:48 +0200

    network integration libs and code

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -2,6 +2,8 @@ object Libs {
 
     val androidLibs = AndroidLibs
     val materialDesignLibs = MaterialDesignLibs
+    val networkLibs = NetworkLibs
+    val gsonLibs = GsonLibs
 
     object AndroidLibs {
         const val core = "androidx.core:core-ktx:${Versions.appCompat}"
@@ -14,13 +16,32 @@ object Libs {
         const val material = "com.google.android.material:material:${Versions.material}"
     }
 
+    object NetworkLibs {
+        const val retrofitRuntime = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        const val retrofitAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+        const val retrofitConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+
+        //okHttp
+        const val loggingInterceptor =
+            "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}"
+    }
+
+    object GsonLibs {
+        const val gsonGoogle = "com.google.code.gson:gson:${Versions.gsonGoogle}"
+    }
+
     object Versions {
 
+        //android core
         const val core = "1.6.0"
         const val appCompat = "1.3.1"
         const val material = "1.4.0"
         const val constraintLayout = "2.1.0"
 
+        //network
+        const val retrofit = "2.9.0"
+        const val loggingInterceptor = "4.9.1"
+        const val gsonGoogle = "2.8.7"
     }
 }
 
---
+++ common/data/src/main/java/com/app/data/netowrk/InterceptorBuilder.kt
@@ -0,0 +1,18 @@
+package com.app.data.netowrk
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+class InterceptorBuilder(private val tokenInterceptor: TokenInterceptor) {
+
+    fun build(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val builder = OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
+            .addInterceptor(tokenInterceptor)
+
+        return builder.build()
+    }
+}
